a5ee26c96f865ae069625395564d8d04f9c3e7f8
Nicolas Manjarres Gonzalez <nicolasmanjarresg@gmail.com>
2025-01-28 22:24:49 -0500

    practica de log4j y slf4j

--- Modulo3/Clase02/clientes-pedidos/src/main/java/com/devsenior/nmanja/GestionPedidos.java
+++ Modulo3/Clase02/clientes-pedidos/src/main/java/com/devsenior/nmanja/GestionPedidos.java
@@ -3,7 +3,12 @@ package com.devsenior.nmanja;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class GestionPedidos {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GestionPedidos.class);
 
     private List<Pedidos> pedidos;
     private GestionClientes clientes;
@@ -11,6 +16,8 @@ public class GestionPedidos {
     
 
     public GestionPedidos(GestionClientes clientes) {
+
+        LOG.debug("Creando una instancia de gestion de pedidos");
         pedidos = new ArrayList<>();
         this.clientes = clientes;
     }
@@ -18,18 +25,24 @@ public class GestionPedidos {
     public void crearPedido(String clienteId, String producto, int cantidad) throws PedidoInvalidoException {
 
         
+        LOG.trace("Llamando a crear pedido");
+        LOG.debug("Los parametros son {} {} {}", new Object[] {clienteId,producto,cantidad});
 
         try {
             var cliente =clientes.buscarCliente(clienteId);
+            LOG.debug("El cliente es: {}", cliente);
         } catch (ClienteNoEncontradoException e) {
+            LOG.warn("Cliente no existe: {}",clienteId);
             throw new PedidoInvalidoException("No se encontro el cliente");
         }
 
         if(producto.isEmpty()){
+            LOG.warn("Producto esta vacio");
             throw new PedidoInvalidoException("El producto no puede estar vacio");
         }
 
         if(cantidad <=0){
+            LOG.warn("La cantidad es menos que 0");
             throw new PedidoInvalidoException("La cantidad debe ser mayor a 0");
 
         }
@@ -37,11 +50,16 @@ public class GestionPedidos {
 
 
         var pedido = new Pedidos(clienteId, producto, cantidad);
+        LOG.debug("Agregando pedido al sistema: {}",producto);
         pedidos.add(pedido);
+        LOG.info("Pedido agregado con exito");
 
     }
 
-    public Pedidos buscarPedido(String clienteId, String producto){
+    public Pedidos buscarPedido(String clienteId, String... producto){
+
+        LOG.trace("Ingreso a buscar pedido");
+        LOG.debug("Ingresa con los datos {} {}", clienteId, producto);
 
         try {
             var cliente = clientes.buscarCliente(clienteId);
@@ -59,6 +77,8 @@ public class GestionPedidos {
             }
             
         }
+
+        LOG.warn("El pedido no existe");
 
         return null;
 
--- Modulo3/Clase02/clientes-pedidos/src/main/java/com/devsenior/nmanja/Main.java
+++ Modulo3/Clase02/clientes-pedidos/src/main/java/com/devsenior/nmanja/Main.java
@@ -1,24 +1,49 @@
 package com.devsenior.nmanja;
 
+import org.apache.logging.log4j.LogManager;
+
 public class Main {
     public static void main(String[] args) {
+
+         var log = LogManager.getLogger(Main.class);   //Llamamos al administrador de Logger, llamamos el método get y colocamos como parámetro Main.class
+
+         log.info("Inicia la ejecucion del programa");
+
+
         var clientes = new GestionClientes();
 
+        log.debug("Registrando el cliente (1, Nicolas)");
         clientes.registrarCliente("1", "Nicolas");
+        log.info("Se creo el cliente Nicolas");
+
+        log.debug("Registrando el cliente (2, Dayana)");
         clientes.registrarCliente("2", "Dayana");
+        log.info("Se creo el cliente Nicolas");
+
+        log.debug("Registrando el cliente (3, Cerati)");
         clientes.registrarCliente("3", "Cerati");
+        log.info("Se creo el cliente Cerati");
 
         var pedidos = new GestionPedidos(clientes);
 
         try{
 
+            
+        log.debug("Creando el pedido (1, perro, 4)");
         pedidos.crearPedido("1", "Perro", 4);
+        log.debug("Creando el pedido (2, manzana, 8)");
         pedidos.crearPedido("2", "Manzada", 8);
+        log.debug("Creando el pedido (5, panela, 10)");
         pedidos.crearPedido("5", "Panela", 10);
         } catch(PedidoInvalidoException e){
+            log.warn("No se pudo agregar uno de los pedidos",e);
             System.out.println("Ocurrio un error al gestionar el pedido\n\t" + e.getMessage());
         }
 
         System.out.println("Gracias por usar nuestro servicio");
+
+
+        log.info("Salio de la aplicacion");
+
     }
 }
