d485fd9e3c22ceb36884df195d56788deb0de592
Nicolas Manjarres Gonzalez <nicolasmanjarresg@gmail.com>
2025-05-07 11:46:47 -0500

    modulo 6

---
+++ Modulo6/demo-tutoria1-modulo6/src/main/java/com/devsenior/nmanja/demo_tutoria1_modulo6/DemoTutoria1Modulo6Application.java
@@ -0,0 +1,13 @@
+package com.devsenior.nmanja.demo_tutoria1_modulo6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTutoria1Modulo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTutoria1Modulo6Application.class, args);
+	}
+
+}
---
+++ Modulo6/demo-tutoria1-modulo6/src/test/java/com/devsenior/nmanja/demo_tutoria1_modulo6/DemoTutoria1Modulo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenior.nmanja.demo_tutoria1_modulo6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTutoria1Modulo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
