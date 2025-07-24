64e38dd3b74b82b79527384a2f23f6a5ea99b3b7
Manjarres <nicolasmanjarresg@gmail.com>
2025-07-24 12:26:15 -0500

    angular modulo 8 clase 3

--- Modulo8/real-state-frontend/src/app/app.ts
+++ Modulo8/real-state-frontend/src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { PropertyList } from './features/properties/component/property-list/property-list';
-import { SignalSample } from './features/samples/signal/signal-sample';
-import { Lifecycle } from './features/samples/lifecycle/lifecycle';
+
 
 @Component({
   selector: 'app-root', //Etiqueta en HTML
-  imports: [PropertyList, SignalSample, Lifecycle], 
+  imports: [PropertyList], 
   templateUrl: './app.html', //Plantilla
   styleUrl: './app.css' //Plantilla
 })
--- Modulo8/real-state-frontend/src/app/features/properties/component/property-list/property-list.ts
+++ Modulo8/real-state-frontend/src/app/features/properties/component/property-list/property-list.ts
@@ -1,85 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, effect, OnInit, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Property } from '../../model/property.model';
 import { CurrencyPipe } from '@angular/common';
+import { PropertyCard } from '../property-card/property-card';
+import { PropertyService } from '../../service/propertyService';
 
 @Component({
   selector: 'app-property-list',
-  imports: [CurrencyPipe],
+  imports: [PropertyCard, FormsModule],
   templateUrl: './property-list.html',
   styleUrl: './property-list.css'
 })
-export class PropertyList {
+export class PropertyList implements OnInit{
 
-  properties: Property[] = [];
-  loading: boolean =  false;
-  error: string | null = null;
+  protected readonly title = ('Nicolas Real State');
 
-  constructor(){
+  allProperties = signal<Property[]>([]);
+  properties = signal<Property[]>([]);
+  loading = signal<boolean>(false);
+  error = signal<string | undefined>(undefined);
+
+  filterCity = signal<string>("");
+
+  constructor( private service : PropertyService ){
+    effect(() => {
+      if(!this.filterCity()){
+        this.properties.set(this.allProperties());
+      } else{
+        this.properties.set(this.allProperties()
+          .filter((p) => p.city.toLowerCase().includes(this.filterCity().toLowerCase()))
+      );
+      }
+
+    })
+  }
+
+
+  ngOnInit(): void {
     this.loadProperties();
   }
 
-  loadProperties(): void {
-    this.loading = true;
-    this.error = null;
+  showDetail(id: number){
 
-    setTimeout(() => {
-      this.properties = [
-      {
-        id: 1,
-        address: '123 Main St',
-        city: 'New York',
-        price: 950000,
-        bedrooms: 3,
-        bathrooms: 2,
-        imageUrl: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=80',
-        description: 'Hermoso departamento en el corazón de Manhattan, cerca de Central Park.'
-      },
-      {
-        id: 2,
-        address: '456 Ocean Drive',
-        city: 'Miami',
-        price: 1200000,
-        bedrooms: 4,
-        bathrooms: 3,
-        imageUrl: 'https://images.unsplash.com/photo-1460518451285-97b6aa326961?auto=format&fit=crop&w=800&q=80',
-        description: 'Casa moderna frente al mar con piscina privada y acceso directo a la playa.'
-      },
-      {
-        id: 3,
-        address: '789 Sunset Blvd',
-        city: 'Los Angeles',
-        price: 2100000,
-        bedrooms: 5,
-        bathrooms: 4,
-        imageUrl: 'https://images.unsplash.com/photo-1507089947368-19c1da9775ae?auto=format&fit=crop&w=800&q=80',
-        description: 'Mansión de lujo en Beverly Hills con vistas panorámicas de la ciudad.'
-      },
-      {
-        id: 4,
-        address: '321 Maple Ave',
-        city: 'Chicago',
-        price: 670000,
-        bedrooms: 3,
-        bathrooms: 2,
-        imageUrl: 'https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?auto=format&fit=crop&w=800&q=80',
-        description: 'Acogedora casa familiar en un barrio tranquilo, cerca de parques y escuelas.'
-      },
-      {
-        id: 5,
-        address: '654 Mountain Rd',
-        city: 'Denver',
-        price: 850000,
-        bedrooms: 4,
-        bathrooms: 3,
-        imageUrl: 'https://images.unsplash.com/photo-1523217582562-09d0def993a6?auto=format&fit=crop&w=800&q=80',
-        description: 'Chalet con vistas a las montañas, ideal para los amantes de la naturaleza.'
-      }
+    this.service.getProperty(id)
+      .then (prop => alert('Han seleccionado la propiedad: '+ JSON.stringify(prop)))
+      .catch(error => this.error.set(error))
 
-      ];
+    
+  }
 
-      this.loading=false;
+  searchProperties(){
+/*     if(!this.filterCity){
+      this.properties = this.allProperties;
+    } else{
+      this.properties = this.allProperties
+        .filter(p => p.city.toLowerCase().includes(this.filterCity.toLowerCase()));
+    } */
+  }
 
-    }, 1500)
+  private loadProperties(): void {
+
+    
+    this.loading.set(true);
+    this.error.set(undefined);
+
+    this.service.getAllProperties()
+        .then(data => {
+
+          
+          this.allProperties.set(data);
+          this.properties.set(this.allProperties());
+          this.loading.set(false)
+
+          
+        })
+        .catch(error =>
+          this.error.set(error));
+
   }
 
 }
