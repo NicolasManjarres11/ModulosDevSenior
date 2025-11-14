a23662c1124cd918e98733472b024b9aadf02421
Manjarres <nicolasmanjarresg@gmail.com>
2025-11-13 20:12:44 -0500

    actualización dev senior

--- Modulo8.2/properties-frontend/src/app/app.ts
+++ Modulo8.2/properties-frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PropertyList } from "./features/properties/components/property-list/property-list";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [PropertyList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
